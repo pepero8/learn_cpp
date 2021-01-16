243ab91d52fb3595ea64d86e644ecd97d878e100
pepero8 <58838808+pepero8@users.noreply.github.com>
2021-01-16 23:19:08 +0900

    Update class.cpp

--- OOP/class.cpp
+++ OOP/class.cpp
@@ -16,12 +16,69 @@ class Person {
 		}
 };
 
+class Animal {
+	int age_;
+	int hunger_;
+	int clean_;
+	
+	public:
+		/*constructor*/
+		Animal(int age, int hunger, int clean) {
+			age_ = age;
+			hunger_ = hunger;
+			clean_ = clean;
+		}
+		
+		Animal() {
+			age_ = 1;
+			hunger_ = 0;
+			clean_ = 100;
+		}
+		
+		void setStat(int age, int hunger, int clean);
+		void showStat();
+};
+
+class Test {
+	public:
+		Test() = default;
+		void test() {
+			std::cout << "Test created" << std::endl;
+		}
+};
+
+void Animal::setStat(int age, int hunger, int clean) {
+	age_ = age;
+	hunger_ = hunger;
+	clean_ = clean;
+}
+
+void Animal::showStat() {
+	std::cout << "age: " << age_ << "\nhunger: " << hunger_ << "\nclean: " << clean_ << std::endl;
+}
+
 int main(int argc, char** argv) {
 	/*basic class practice*/
 	Person person;
+	Animal animal1(1, 0, 100);
+	Animal animal2 = Animal(2, 50, 50);
+	Animal animal3;
 	
 	person.set_person("pepero", 99);
 	person.print_person();
+	
+	std::cout << std::endl;
+	
+	animal1.showStat();
+	std::cout << std::endl;
+	animal2.showStat();
+	std::cout << std::endl;
+	animal3.setStat(1, 0, 80);
+	animal3.showStat();
+	std::cout << std::endl;
+	
+	Test test;
+	test.test();
 	
 	return 0;
 }
