a629353fc4b393c8771fcfbecfb765bd7eed6644
pepero8 <58838808+pepero8@users.noreply.github.com>
2021-02-08 22:04:44 +0900

    Create inheritance.cpp

---
+++ OOP/inheritance.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+class Base {
+	protected:
+		int parent_var;
+	
+	public:
+		int var;
+		
+		Base(): var(10) {
+			function();
+		}
+		
+		void function() {
+			std::cout << var << std::endl;
+		}
+};
+
+class Derived : public Base {
+	int var;
+	
+	public:
+		Derived(): Base() {
+			var = 20;
+			function();
+		}
+};
+
+class Derived2 : public Base {
+	int var;
+	
+	public:
+		Derived2(): Base() {
+			var = 20;
+			function();
+		}
+		
+		/*Overriding*/
+		void function() {
+			std::cout << var << std::endl;
+		}
+		
+		void change() {
+			parent_var = 30;
+			std::cout << parent_var << std::endl;
+		}
+};
+
+class Derived3 : private Base {
+	public:
+		Derived3() : Base() {}
+};
+
+int main(int argc, char** argv) {
+	/*basic inheritance practice1*/
+	std::cout << "--Practice1--" << std::endl;
+	Derived d;
+	
+	std::cout << std::endl;
+	
+	/*basic inheritance practice2*/
+	std::cout << "--Practice2--" << std::endl;
+	Derived2 d2;
+	
+	std::cout << std::endl;
+	
+	/*basic inheritance practice3*/
+	std::cout << "--Practice3--" << std::endl;
+	d2.change();
+	
+	/*basic inheritance practice4*/
+	std::cout << "--Practice4--" << std::endl;
+	Base b;
+	Derived3 d3;
+	std::cout << b.var << std::endl;
+	//std::cout << d3.var << std::endl; -> var inaccessible
+	
+	return 0;
+}
